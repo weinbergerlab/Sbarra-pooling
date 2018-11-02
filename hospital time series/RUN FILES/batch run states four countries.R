b01e61a8ad893bba7212d3f06b510207b697efe1
weinbergerlab <dweinber@gmail.com>
2018-11-02 11:00:40 -0400

    update batch file

--- hospital time series/RUN FILES/batch run states four countries.R
+++ hospital time series/RUN FILES/batch run states four countries.R
@@ -1,4 +1,10 @@
-source('C:/Users/dmw63/Documents/GitHub/synthetic-control-poisson/synthetic_control_run_Mexico_state.R')
-source('C:/Users/dmw63/Documents/GitHub/synthetic-control-poisson/synthetic_control_run_Brazil_state.R')
-source('C:/Users/dmw63/Documents/GitHub/synthetic-control-poisson/synthetic_control_run_Ecuador_state.R')
-source('C:/Users/dmw63/Documents/GitHub/synthetic-control-poisson/synthetic_control_run_Chile_region.R')
+#To run these, open the synthetic-control-poisson git directory
+
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run_Mexico_state.R')
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run_Brazil_state.R')
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run_Ecuador_state.R')
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run_Chile_region.R')
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run Fiji hosp.R')
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run US hosp.R')
+source('C:/Users/dmw63/Weinberger Lab Dropbox/Dan Weinberger/pooling github/Sbarra-pooling/hospital time series/RUN FILES/synthetic_control_run Denmark hosp.R')
+
