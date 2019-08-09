75cc33987345406b616d33ad480ec1a9e1799ca4
weinbergerlab <dweinber@gmail.com>
2019-08-09 16:18:13 -0400

    delete unneeded output files

--- first stage estimates/batch run.R
+++
@@ -1,5 +0,0 @@
-# source("C:/Users/dmw63/Dropbox (Personal)/meta_analysis_results/31 aug automated read in with pooling all states.R")
-# source("C:/Users/dmw63/Dropbox (Personal)/meta_analysis_results/31 aug automated read in NO pooling all states.R")
-source('C:/Users/dmw63/Dropbox (Personal)/meta_analysis_results/stack all states/B_spline with pooling/31 aug automated read in with pooling all states b splines.R')
-source('C:/Users/dmw63/Dropbox (Personal)/meta_analysis_results/stack all states/B_spline no pooling/31 aug automated read in no pooling all states b splines.R')
-
